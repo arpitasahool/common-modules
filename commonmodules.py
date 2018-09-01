51b085a48a4a1f4953d76239af6bfb655123c284
arpitasahool <arpitasahoo945@gmail.com>
2018-09-01 08:44:01 +0530

    the problem is resolved

---
+++ commonmodules.py
@@ -0,0 +1,13 @@
+#Q.1- Print the current day using Datetime module.
+
+from datetime import date
+x=date.today()
+print(x.day)
+
+#Q.2- Open your browser and play a video using webbrowser module in python.
+
+import webbrowser
+webbrowser.open("https://www.youtube.com/watch?v=kg1BljLu9YY")
+print()
+
+#Q.3- Write a program to rename all the files in a directory and convert them into .jpg file format.
